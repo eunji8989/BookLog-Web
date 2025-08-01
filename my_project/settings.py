bff33aedc79d76499de8266c34a3e01383c457d7
신은지 <193382637+eunji8989@users.noreply.github.com>
2025-08-01 13:18:05 +0900

    최종 커밋

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'pages',
     'accounts.apps.AccountsConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- product/models.py
+++ product/models.py
@@ -5,6 +5,7 @@ class MainContent(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
+    image = models.ImageField(upload_to='maincontent_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
